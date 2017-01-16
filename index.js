b822eff9818aa8033c974c1ef1639e6976efc490
Paul Adams <adamspe@gmail.com>
2017-01-16 15:49:57 -0600

    start pre-process from input file.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var AppContainer = require('app-container'),
     appModule = {
         init: function(container) {
             var resources = require('./api');
-            return container.addResource(resources.featuresResource).addResource(resources.layersResource);
+            return container.addResource(resources.featuresResource)
+                            .addResource(resources.layersResource);
         },
         container: function(initPipeline) {
             return appModule.init(new AppContainer().init(initPipeline));
---
+++ layer-import/index.js
@@ -0,0 +1,2 @@
+
+module.exports.LayerPreProcessor = require('./pre/LayerPreProcessor');
