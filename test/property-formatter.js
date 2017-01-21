7b658a726a9601c36602e1446f12616940d81f24
Paul Adams <adamspe@gmail.com>
2017-01-21 14:04:08 -0600

    add property-formatter

---
+++ test/property-formatter.js
@@ -0,0 +1,50 @@
+var should = require('should'),
+    _ = require('lodash'),
+    PropertyFormatter = require('../lib/property-formatter');
+
+describe('Property Formatter',function(){
+    it('basic',function(done){
+        var pf = new PropertyFormatter('%s[: %s],A,B');
+        pf.format({
+            A: 'foo',
+            B: 'bar'
+        }).should.equal('foo: bar');
+        pf.format({
+            A: 'foo'
+        }).should.equal('foo');
+        done();
+    });
+
+    it('number',function(done) {
+        (new PropertyFormatter('%s: %d,a,b')).format({
+            a: 'foo',
+            b: 10
+        }).should.equal('foo: 10');
+        done();
+    });
+
+    it('negative: fmt mismatch',function(done) {
+        try {
+            var p = new PropertyFormatter('%s[: %s][: %s],a,b');
+            should.fail('expected error on mis-match');
+        } catch(error) {
+            done();
+        }
+    });
+
+    it('negative: missing required',function(done) {
+        try {
+            var p = new PropertyFormatter('%s[: %s],a,b');
+            p.format({
+                a: 'foo',
+                b: 'bar'
+            }).should.equal('foo: bar');
+            p.format({
+                b: 'bar'
+            });
+            should.fail('expected error on missing required key');
+        } catch(error) {
+            done();
+        }
+    });
+});
